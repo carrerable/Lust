921429f4e8a6853bbfb5b6fb65a62c54c932099a
VengalaMx <66300003+VengalaMx@users.noreply.github.com>
2021-01-02 17:44:27 -0600

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -88,7 +88,7 @@ $scope.authStateListener = function(user) {
 $scope.googleRegistro = function (){
 	console.log("Entro al registro de google ")
 
-firebase.auth().signInWithPopup($scope.provider).then(function(result) {
+firebase.auth().signInWithRedirect($scope.provider).then(function(result) {
   // This gives you a Google Access Token. You can use it to access the Google API.
   var token = result.credential.accessToken;
   // The signed-in user info.
