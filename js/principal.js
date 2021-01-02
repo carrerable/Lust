ccb2178d54a081d26f17ac4690265f24dff3cb90
VengalaMx <66300003+VengalaMx@users.noreply.github.com>
2021-01-02 17:41:51 -0600

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -92,7 +92,9 @@ firebase.auth().signInWithPopup($scope.provider).then(function(result) {
   // This gives you a Google Access Token. You can use it to access the Google API.
   var token = result.credential.accessToken;
   // The signed-in user info.
+  console.log(token)
   var user = result.user;
+  console.log(user)
   // ...
 }).catch(function(error) {
   // Handle Errors here.
